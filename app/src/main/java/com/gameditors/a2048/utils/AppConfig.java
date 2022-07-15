effdc83c6c0d8091748d834b25668f0fcafa467b
Doozy <dozdoozie@gmail.com>
2022-07-15 15:47:07 +0300

    put ads

---
+++ app/src/main/java/com/gameditors/a2048/utils/AppConfig.java
@@ -0,0 +1,13 @@
+package com.gameditors.a2048.utils;
+
+public class AppConfig {
+    // force rtl layout direction
+    public static final boolean RTL_LAYOUT = false;
+
+    // flag for display ads, change true and false only
+    public static final boolean ADS_ALL_ENABLE = true;
+
+    public static final boolean ADS_MAIN_BANNER = ADS_ALL_ENABLE && true;
+
+
+}
